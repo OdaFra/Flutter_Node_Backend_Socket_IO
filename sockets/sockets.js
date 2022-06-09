ba12b54103fea996f6ba2816eed7941493cd4093
Oscar Ramirez <oscar.ramirez@itti.digital>
2022-06-09 15:40:09 -0400

    Desarrollo de seccion 4 hasta el capitulo 35

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,7 +1,23 @@
-const {io}=require('../index');
+const { io } = require('../index');
+
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Scorpion' ) );
+bands.addBand( new Band( 'Bon Jovi' ) );
+bands.addBand( new Band( 'Guns' ) );
+bands.addBand( new Band( 'Metallica' ) );
+
+console.log(bands);
+
+console.log('init servers')
 //Mensaje de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -10,5 +26,11 @@ io.on('connection', client => {
         console.log('mensaje', payload);
         io.emit('mensaje', { admin: 'David' });
     });
+
+    client.on('emitir-mensaje', (payload)=>{
+      //  console.log(payload)
+      client.broadcast.emit('emitir-mensaje', payload);
+    });
+
 
 });
